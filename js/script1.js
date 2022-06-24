589be8b51dc6c6bf531fcb51dd3c373f0042006b
tsanevp <78775944+tsanevp@users.noreply.github.com>
2022-06-24 14:11:31 -0700

    Working on main home page and adding header button values

---
+++ js/script1.js
@@ -0,0 +1,19 @@
+$('#AboutMe').click(function() {
+    window.location='AboutMe.html';
+});
+
+$('#Projects').click(function() {
+    window.location='Projects.html';
+});
+
+$('#Resume').click(function() {
+    window.location='https://drive.google.com/file/d/1E_ClEYxCbqXSlmUSKDwRmD3upcLa59T8/view?usp=sharing';
+});
+
+$('#ContactMe').click(function() {
+    window.location='ContactMe.html';
+});
+
+$('#homeImg').click(function() {
+    window.location='index.html';
+});
